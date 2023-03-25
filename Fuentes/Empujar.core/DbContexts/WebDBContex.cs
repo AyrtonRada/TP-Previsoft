fef1a58dea13d56fee99a5a4edc010a424a1206f
AyrtonRada <axel582011@hotmail.com>
2023-03-25 02:41:54 -0300

    Base proyecto

---
+++ Fuentes/Empujar.core/DbContexts/WebDBContex.cs
@@ -0,0 +1,24 @@
+﻿using Adrian.core.Config;
+using Adrian.core.Models.CONF;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Adrian.core.DbContexts
+{
+    public class WebDBContext : DbContext
+    {
+        /*GRAL*/
+
+
+        /*CONF*/
+        public DbSet<TipoDeCentroDeCosto> TiposDeCentroDeCosto { get; set; }
+
+        /*OPER*/
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConfigDB.MainDBConnectionString);
+        }
+
+    }
+}
